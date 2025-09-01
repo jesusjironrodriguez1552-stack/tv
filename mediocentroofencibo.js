9d7e4bed83dcd65fbd011f5b1a2589b33bb8ec62
alexander99 <jesusjironrodriguez155.2@gmail.com>
2025-08-31 23:01:30 -0500

    Create mediocentroofencibo.js

---
+++ mediocentroofencibo.js
@@ -0,0 +1,114 @@
+const mediocentrosOfensivos = [
+  // ---- TOP MEDIOCENTROS OFENSIVOS ----
+  {
+    nombre: "Kevin",
+    apellido: "De Bruyne",
+    nacionalidad: "Bélgica",
+    club: "Manchester City",
+    precioTransferencia: 95
+  },
+  {
+    nombre: "Bruno",
+    apellido: "Fernandes",
+    nacionalidad: "Portugal",
+    club: "Manchester United",
+    precioTransferencia: 85
+  },
+  {
+    nombre: "Martin",
+    apellido: "Ødegaard",
+    nacionalidad: "Noruega",
+    club: "Arsenal",
+    precioTransferencia: 90
+  },
+  {
+    nombre: "Jamal",
+    apellido: "Musiala",
+    nacionalidad: "Alemania",
+    club: "Bayern Múnich",
+    precioTransferencia: 100
+  },
+  {
+    nombre: "Bernardo",
+    apellido: "Silva",
+    nacionalidad: "Portugal",
+    club: "Manchester City",
+    precioTransferencia: 80
+  },
+
+  // ---- GANGAS (BUENOS Y BARATOS) ----
+  {
+    nombre: "Florian",
+    apellido: "Wirtz",
+    nacionalidad: "Alemania",
+    club: "Bayer Leverkusen",
+    precioTransferencia: 35
+  },
+  {
+    nombre: "Dominik",
+    apellido: "Szoboszlai",
+    nacionalidad: "Hungría",
+    club: "Liverpool",
+    precioTransferencia: 30
+  },
+  {
+    nombre: "Paquetá",
+    apellido: "",
+    nacionalidad: "Brasil",
+    club: "West Ham",
+    precioTransferencia: 28
+  },
+  {
+    nombre: "Enzo",
+    apellido: "Le Fée",
+    nacionalidad: "Francia",
+    club: "Rennes",
+    precioTransferencia: 20
+  },
+  {
+    nombre: "Xavi",
+    apellido: "Simons",
+    nacionalidad: "Países Bajos",
+    club: "RB Leipzig",
+    precioTransferencia: 25
+  },
+
+  // ---- MALOS / SOBREVALORADOS ----
+  {
+    nombre: "James",
+    apellido: "Rodríguez",
+    nacionalidad: "Colombia",
+    club: "São Paulo",
+    precioTransferencia: 40 // ya no es el mismo de antes, precio alto
+  },
+  {
+    nombre: "Philippe",
+    apellido: "Coutinho",
+    nacionalidad: "Brasil",
+    club: "Al Duhail",
+    precioTransferencia: 35 // sobrevalorado por su pasado en Barça/Liverpool
+  },
+  {
+    nombre: "Dele",
+    apellido: "Alli",
+    nacionalidad: "Inglaterra",
+    club: "Everton",
+    precioTransferencia: 25 // nivel muy bajo en los últimos años
+  },
+  {
+    nombre: "Oscar",
+    apellido: "",
+    nacionalidad: "Brasil",
+    club: "Shanghai Port",
+    precioTransferencia: 33 // en liga menos competitiva, precio engañoso
+  },
+  {
+    nombre: "Isco",
+    apellido: "Alarcón",
+    nacionalidad: "España",
+    club: "Real Betis",
+    precioTransferencia: 30 // rendimiento irregular, precio elevado
+  }
+];
+
+console.log(mediocentrosOfensivos);
